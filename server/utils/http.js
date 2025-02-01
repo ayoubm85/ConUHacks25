561e620410c96fb80fc880199e6061b3acb6d925
Ayoub Marfouk <il>
2025-02-01 17:14:27 -0500

    Initial commit

---
+++ server/utils/http.js
@@ -0,0 +1,19 @@
+/**
+ * HTTP status codes
+ */
+const HTTP_STATUS =
+{
+    SUCCESS: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    REDIRECT: 302,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500
+};
+
+module.exports = {
+    HTTP_STATUS
+};
